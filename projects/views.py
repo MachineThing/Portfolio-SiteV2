cae2eac5ac1c88d794028cc173276f5ba9b28c5c
Mason Fisher <sackzombie10@gmail.com>
2021-02-10 12:19:22 -0500

    Made age dynamic

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
+from datetime import datetime
+from pytz import timezone
 
 def index(request):
-    # TODO: make age dynamic
-    return render(request, 'projects/index.html', {'age': 16})
+    birth_date = datetime(2004, 3, 17)
+    today_date = datetime.now(timezone(settings.TIME_ZONE))
+    age = today_date.year - birth_date.year - ((today_date.month, today_date.day) < (birth_date.month, birth_date.day))
+
+    return render(request, 'projects/index.html', {'age': age})

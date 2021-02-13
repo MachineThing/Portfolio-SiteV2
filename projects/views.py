afd73fcc9aebf20c5f801e451315b3a74a5f714b
Mason Fisher <sackzombie10@gmail.com>
2021-02-12 22:24:41 -0500

    Drew rounded squares

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.conf import settings
 from datetime import datetime
 from pytz import timezone
+from PIL import ImageDraw
 from io import BytesIO
 from PIL import Image
 import requests
@@ -37,11 +38,48 @@ def contribCal(request):
     contribCal = requests.post('https://api.github.com/graphql', json={'query':ghScript.replace("\n", "")}, headers=head)
 
     # Image settings
-    square_size = 25
-    square_margin = 12
+    square_size = 30
+    square_margin = 12 # Multiplied by 2
     weeks = 52
+    color = (0, 200, 0)
+    maxt = 128
 
-    img = Image.new("RGBA", ((square_size+square_margin)*weeks, (square_size+square_margin)*7), (255, 0, 0, 136))
+    img = Image.new("RGBA", ((square_size+square_margin)*weeks+square_margin, (square_size+square_margin)*7+square_margin), (0, 0, 0, 136))
+    imgd = ImageDraw.Draw(img)
+
+    # Draw rounded squares
+    for squarex in range(square_margin, (square_size+square_margin)*weeks, square_size+square_margin):
+        for squarey in range(square_margin, (square_size+square_margin)*7, square_size+square_margin):
+            imgd.rectangle( # X Rectangle
+                (squarex, squarey+(square_size/4),
+                squarex+square_size, squarey+square_size-(square_size/4)),
+                fill=(color[0], color[1], color[2], maxt)
+            )
+            imgd.rectangle( # Y Rectangle
+                (squarex+(square_size/4), squarey,
+                squarex+square_size-(square_size/4), squarey+square_size),
+                fill=(color[0], color[1], color[2], maxt)
+            )
+            imgd.ellipse( # Upper-Left Circle
+                (squarex, squarey,
+                squarex+square_size/2, squarey+square_size/2),
+                fill=(color[0], color[1], color[2], maxt)
+            )
+            imgd.ellipse( # Upper-Right Circle
+                (squarex+square_size/2, squarey,
+                squarex+square_size, squarey+square_size/2),
+                fill=(color[0], color[1], color[2], maxt)
+            )
+            imgd.ellipse( # Lower-Left Circle
+                (squarex, squarey+square_size/2,
+                squarex+square_size/2, squarey+square_size),
+                fill=(color[0], color[1], color[2], maxt)
+            )
+            imgd.ellipse( # Lower-Right Circle
+                (squarex+square_size/2, squarey+square_size/2,
+                squarex+square_size, squarey+square_size),
+                fill=(color[0], color[1], color[2], maxt)
+            )
     imgBuff = BytesIO()
     img.save(imgBuff, "PNG")
     return HttpResponse(imgBuff.getvalue(), content_type='image/png')

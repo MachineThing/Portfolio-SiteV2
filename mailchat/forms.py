3b521d0c3bd2489a175efdc7e611a379aa37b724
Mason Fisher <sackzombie10@gmail.com>
2021-07-08 21:37:38 -0400

    Formatted country list
    Changes:
    - Moved "United States" and "United Kingdom" to the top of the list
    - Removed "Antarctica" and "North Korea" from the country list
    - Added placeholder "Please select a country" entry so the user has to 
    put in a country else it will not register

--- mailchat/forms.py
+++ mailchat/forms.py
@@ -1,11 +1,29 @@
 from django.forms import Form
+from unidecode import unidecode
 import country_list
 from . import fields
+
+def return_countries():
+    countries = country_list.countries_for_language('EN')[:] # Copy the list to avoid modifying the master list
+    filter_list = lambda my_list, target:my_list.index(list(filter(lambda iterable:target.upper() in iterable, my_list))[0]) # Find index to the target
+    # Remove "troll" countries
+    troll_countries = ['KP', 'AQ']
+    for count in troll_countries:
+        countries.pop(filter_list(countries, count))
+
+    # Reverse sort (while treating diacritics as normal alphabet or it will go wrong)
+    countries.sort(key=lambda x: unidecode(x[1]).upper(), reverse=True)
+
+    # Add some countries to the front
+    countries += [countries.pop(filter_list(countries, 'GB'))]
+    countries += [countries.pop(filter_list(countries, 'US'))]
+    countries.reverse()
+    return countries
 
 class MailForm(Form):
     name = fields.CharField(label='Name:')
     company = fields.CharField(label='School/Company:')
-    country = fields.SelectField(label='Country:', choices=tuple(country_list.countries_for_language('EN')))
+    country = fields.SelectField(label='Country:', choices=return_countries())
     email = fields.EmailField(label='Email address:')
     subject = fields.CharField(label='Subject:')
     message = fields.CharField(min_length=100, big=True, label='Message:', attrs={'rows':3})
